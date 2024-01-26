f67e6fdca7d8ff28519b8e208742bde9da893436
brajp <brajpandey0584@gmail.com>
2024-01-27 01:01:41 +0530

    commit to get the sum value

---
+++ demo.java
@@ -0,0 +1,13 @@
+/**
+ * demo
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println("sum is equals: "+c);
+    }
+}  
